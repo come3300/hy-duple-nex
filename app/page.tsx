622c5b858378bf0b53a1b73b10d179987df502c3
come3300 <suke3300@gmail.com>
2026-06-12 01:57:57 +0900

    patternBに切り替えるボタンの削除と不要なファイル、コードの削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Link from "next/link";
 import { PatternA } from "./pattern-a";
 
 export default function Home() {
-  return (
-    <>
-      <PatternA />
-      <Link className="floatingSwitch" href="/pattern-b">
-        Pattern B
-      </Link>
-    </>
-  );
+  return <PatternA />;
 }
